6d495c5b8fdb92256bd4456b354c655d30362a46
Eduardo Perucci <peruccii2917@hotmail.com>
2024-07-31 21:09:57 -0300

    FEAT: Constants

---
+++ src/main/java/com/rememberme/rememberMe/config/constants/RabbitMQConstants.java
@@ -0,0 +1,6 @@
+package com.rememberme.rememberMe.config.constants;
+
+public class RabbitMQConstants {
+    public static final String STOCK_QUEUE = "STOCK";
+    public static final String STOCK_EMAIL = "EMAIL";
+}
