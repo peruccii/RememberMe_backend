aa294a0efa1c780b98eabf94b66bf98676bfa6f9
Eduardo Perucci <peruccii2917@hotmail.com>
2024-07-31 21:08:55 -0300

    FEAT: Email sender DTO

--- src/main/java/com/rememberme/rememberMe/dtos/EmailSenderDTO.java
+++ src/main/java/com/rememberme/rememberMe/dtos/EmailSenderDTO.java
@@ -1,10 +1,6 @@
 package com.rememberme.rememberMe.dtos;
 
 public record EmailSenderDTO(
-        String ownerRef,
-        String emailFrom,
-        String emailTo,
-        String subject,
-        String text
+        String to
 ) {
 }
